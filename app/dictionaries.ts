a8fb497d479f9db51f0f5616a02ac1881f52b9cf
Jake <jiovana.jake@gmail.com>
2025-02-21 13:17:09 +0900

    Add english dictionary

--- app/dictionaries.ts
+++ app/dictionaries.ts
@@ -1,10 +1,10 @@
 import "server-only";
 
 const dictionaries = {
-  ["en-US"]: () =>
-    import("@/dictionaries/en-US.json").then((module) => module.default),
   ["ko-KR"]: () =>
     import("@/dictionaries/ko-KR.json").then((module) => module.default),
+  ["en-US"]: () =>
+    import("@/dictionaries/en-US.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: "en-US" | "ko-KR") =>
